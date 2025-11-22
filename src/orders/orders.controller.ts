4c5df35b4cd4edede82675d1caf910a18815dd27
EnriquePlayGroupLatam <enrique@playgrouplatam.com>
2025-11-22 04:52:16 -0500

    96.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -20,38 +20,22 @@ export class OrdersController {
     return this.ordersClient.send('createOrder', createOrderDto);
   }
 
-  // @Get()
-  // findAll(){
-  //   return this.ordersClient.send('findAllOrders', {})
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id',ParseUUIDPipe) id: string){
-  //   return this.ordersClient.send('findOneOrder', {id})
-  // }
-
-  // @Get(':id')
-  // async findOne(@Param('id', ParseUUIDPipe ) id: string) {
-  //   try {
-  //     const order = await firstValueFrom(
-  //       this.ordersClient.send('findOneOrder', { id })
-  //     );
-
-  //     return order;
-
-  //   } catch (error) {
-  //     throw new RpcException(error);
-  //   }
-  // }
-
-  // @Post()
-  // create(@Body() createOrderDto: CreateOrderDto) {
-  //   return this.ordersClient.send('createOrder', createOrderDto);
-  // }
 
   @Get()
-  findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
-    return this.ordersClient.send('findAllOrders', orderPaginationDto);
+  async findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
+
+    try{
+      const orders =  await firstValueFrom(
+        this.ordersClient.send('findAllOrders', orderPaginationDto)
+      )
+      return orders;
+    }catch(error){
+      throw new RpcException(error);
+    }
+    
+
+
+
   }
   
   @Get('id/:id')
